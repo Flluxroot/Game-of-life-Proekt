6b90f4b362e97ad346a3640337a8fe51dcdeb935
esinl <esinl@UWU>
2024-01-22 00:01:01 +0200

    Add project files.

---
+++ Proekt.UPpr_23-24_fn0MI0600404_KP02/Proekt.UPpr_23-24_fn0MI0600404_KP02.cpp
@@ -0,0 +1,20 @@
+/*
+* Solution to course project #02
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author <Esin Myumyun>
+* @idnumber <0MI0600404>
+* @compiler <VC>
+*
+* <Game of life entire file>
+*/
+
+#include <iostream>
+
+int main()
+{
+  
+}
+
